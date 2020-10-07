40d9e842d6be136ab21f728a5e0a060a93658bab
kydyrsha <sanatkydyrsha@gmail.com>
2020-10-07 15:15:29 +0600

    firebase on

---
+++ src/app/admin/shared/services/auth.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../../../shared/interfaces';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {environment} from '../../../../environments/environment';
+import {FbAuthResponse} from '../../../../environments/interface';
+
+@Injectable()
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  get token(): string {
+    return ''
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      )
+  }
+
+  logout() {
+
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token
+  }
+
+  private setToken(response: FbAuthResponse) {
+    console.log(response);
+  }
+}
