fed6817b65f85f2a301c28073a5d9096c2a4054c
kydyrsha <sanatkydyrsha@gmail.com>
2020-10-12 17:02:06 +0600

    edit-page & notifications & localization

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -12,6 +12,8 @@ import {EditPageComponent} from './edit-page/edit-page.component';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
 import {SearchPipe} from './shared/search.pipe';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import {AlertService} from './shared/services/alert.service';
 
 
 @NgModule({
@@ -21,7 +23,8 @@ import {SearchPipe} from './shared/search.pipe';
     DashboardPageComponent,
     CreatePageComponent,
     EditPageComponent,
-    SearchPipe
+    SearchPipe,
+    AlertComponent
   ],
   imports: [
     CommonModule,
@@ -41,7 +44,7 @@ import {SearchPipe} from './shared/search.pipe';
     ])
   ],
   exports: [RouterModule],
-  providers: [AuthGuard]
+  providers: [AuthGuard, AlertService]
 })
 export class AdminModule {
 
--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AuthService} from '../shared/services/auth.service';
 import {Post} from '../../shared/interfaces';
 import {PostsService} from '../../shared/posts.service';
 import {Subscription} from 'rxjs';
+import {AlertService} from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -11,32 +11,36 @@ import {Subscription} from 'rxjs';
 })
 export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  posts: Post[] = []
-  pSub: Subscription
-  dSub: Subscription
-  searchStr: ''
+  posts: Post[] = [];
+  pSub: Subscription;
+  dSub: Subscription;
+  searchStr: '';
 
-  constructor(private postsService: PostsService) { }
+  constructor(
+    private postsService: PostsService,
+    private alert: AlertService
+  ) { }
 
   ngOnInit(): void {
     this.pSub = this.postsService.getAll().subscribe(posts => {
-      this.posts = posts
-    })
+      this.posts = posts;
+    });
   }
 
   remove(id: string) {
     this.dSub = this.postsService.remove(id).subscribe(() => {
-      this.posts = this.posts.filter( post => post.id !== id)
-    })
+      this.posts = this.posts.filter( post => post.id !== id);
+      this.alert.warning('Post is successfully deleted')
+    });
   }
 
   ngOnDestroy() {
     if (this.pSub) {
-      this.pSub.unsubscribe()
+      this.pSub.unsubscribe();
     }
 
     if (this.dSub) {
-      this.dSub.unsubscribe()
+      this.dSub.unsubscribe();
     }
   }
 
