2fd8334f95bd4e8bbb7eefe05d16517b8280acf4
kydyrsha <sanatkydyrsha@gmail.com>
2020-10-12 12:32:21 +0600

    search & delete

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,14 +3,15 @@ import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
-import {AuthService} from './shared/services/auth.service';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
+import {SearchPipe} from './shared/search.pipe';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import {AuthGuard} from './shared/services/auth.guard';
     LoginPageComponent,
     DashboardPageComponent,
     CreatePageComponent,
-    EditPageComponent
+    EditPageComponent,
+    SearchPipe
   ],
   imports: [
     CommonModule,
